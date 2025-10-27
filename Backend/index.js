900658fe984f3f5189b97cf515556a10b0a26e83
jorge nicolino <jgnicolino@gmail.com>
2025-10-27 20:43:33 -0300

    Cambios en index.js

--- Backend/index.js
+++ Backend/index.js
@@ -19,9 +19,9 @@ import sosRoutes from './src/sos/sosRoutes.js';
 import requestToAI from './src/aiModel/aiModelRoute.js';
 // import { loggerMiddleware } from './src/utils/logger.js';
 
-import { loggerMiddleware } from './src/utils/logger.js';
+// import { loggerMiddleware } from './src/utils/logger.js';
 import { startMCP } from './src/mcp/mcp.js';
-import { start } from 'repl';
+// import { start } from 'repl';
 
 
 const app = express();
